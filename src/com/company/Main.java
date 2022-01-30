5b89385ec931c0f75e5d525736a1df286251aecf
mosabzakarneh98 <mosab.zakarneh@harri.com>
2022-01-30 13:24:43 +0200

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
+import java.util.Arrays;
+import java.util.Comparator;
+class G <T> implements Comparator<T>
+{
+
+
+    @Override
+    public int compare(T o1, T o2) {
+        if(o1 instanceof String && o2 instanceof String)
+        {
+         return (((String) o1).length()- ((String) o2).length());
+        }
+        else return ((Integer)o1)-((Integer)o2);
+    }
+}
 
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("mosab");
+    public static void main(String[] args)
+    {
+        Integer I[]={1,5,22,15,12,14,66,40,13,2};
+        String s[]={"mosab","ahmaddf","khddfaled","baxsem"};
+
+        Arrays.sort(I,new G<Integer>());
+        for(int i :I)
+        {
+            System.out.println(i);
+        }
+
+        Arrays.sort(s,new G<String>());
+        for(String i :s)
+        {
+            System.out.println(i);
+        }
+        
+        Arrays.sort(s,(String s1,String s2)->s1.length()-s2.length());
+        for(String i :s)
+        {
+            System.out.println(i);
+        }
     }
 }
